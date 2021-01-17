f52db82be850d7534b9ce99f3041daaf42b3dc70
Fabricio Garcia <gael.fabricio@gmail.com>
2021-01-16 22:15:57 -0600

    ➕feat: Add routing
    * Create component Product
    * Create component NewProduct
    * Create component EditProduct
    * Add react-router-dom and modify App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import Header from './components/Header';
+import Products from './components/Products';
+import NewProduct from './components/NewProduct';
+import EditProduct from './components/EditProduct'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 function App() {
-  return <><Header /></>
+  return (
+    <Router>
+      <Header />
+      <div className="container mt-3">
+        <Switch>
+          <Route exact path="/" component={Products} />
+          <Route exact path="/products/new" component={NewProduct} />
+          <Route exact path="/products/edit/:id" component={EditProduct} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Products.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Products() {
+  return (
+    <div>
+      <h1>From Products.jsx</h1>
+    </div>
+  )
+}
