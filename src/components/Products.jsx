d01582b4ddb3536ffdd7fd167e49d68fa9d26681
Fabricio Garcia <gael.fabricio@gmail.com>
2021-01-17 19:10:26 -0600

    ♻ refactor: Modify App and Edit components

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 
 export default function Products() {
   return (
-    <div>
-      <h1>From Products.jsx</h1>
-    </div>
+    <>
+      <h2 className="text-center my-5">Product List</h2>
+      <table className="table table-stripped">
+        <thead className="bg-primary table-dark">
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Price</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          
+        </tbody>
+      </table>
+    </>
   )
 }
