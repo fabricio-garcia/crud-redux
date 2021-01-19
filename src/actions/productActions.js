c4933d7654277d67160c17f5bf2f849ab6e385e9
Fabricio Garcia <gael.fabricio@gmail.com>
2021-01-18 21:25:51 -0600

    :heavy_plus_sign: feat: Add axios
    * Create axios config file
    * Modify productActions to include axios

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,4 +1,5 @@
 import { ADD_PRODUCT, ADD_PRODUCT_SUCCESS, ADD_PRODUCT_ERROR } from '../types';
+import axiosClient from '../config/axios';
 
 const addProduct = () => ({
   type: ADD_PRODUCT,
@@ -10,19 +11,23 @@ const addProductSuccess = product => ({
   payload: product,
 });
 
-const addProductError = (errorState) => ({
+const addProductError = errorState => ({
   type: ADD_PRODUCT_ERROR,
   payload: errorState,
 });
 
 // Create new products
 export function createNewProductAction(product) {
-  return dispatch => {
+  return async dispatch => {
     dispatch(addProduct());
 
     try {
+      // Pass data to API
+      await axiosClient.post('/products', product);
+      // Pass data to reducer if everything is OK
       dispatch(addProductSuccess(product));
     } catch (error) {
+      console.error(error);
       dispatch(addProductError(true));
     }
   };
