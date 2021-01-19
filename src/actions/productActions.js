caf54c956ab35bb82c305549945bf321148aa8e5
Fabricio Garcia <gael.fabricio@gmail.com>
2021-01-18 18:22:08 -0600

    :memo: feat: Modify action and reducer for NewProduct component

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,12 +1,26 @@
-import {
-  ADD_PRODUCT,
-  ADD_PRODUCT_SUCCESS,
-  ADD_PRODUCT_ERROR,
-} from "../types";
+import { ADD_PRODUCT, ADD_PRODUCT_SUCCESS, ADD_PRODUCT_ERROR } from '../types';
+
+const addProduct = () => ({
+  type: ADD_PRODUCT,
+  payload: true,
+});
+
+const addProductSuccess = product => ({
+  type: ADD_PRODUCT_SUCCESS,
+  payload: product,
+});
+
+const addProductError = () => ({});
 
 // Create new products
 export function createNewProductAction(product) {
-  return () => {
-    console.log(product);
-  }
-};
+  return dispatch => {
+    dispatch(addProduct());
+
+    try {
+      dispatch(addProductSuccess(product));
+    } catch (error) {
+      dispatch(addProductError(true));
+    }
+  };
+}
--- src/reducers/productsReducer.js
+++ src/reducers/productsReducer.js
@@ -1,8 +1,4 @@
-import {
-  ADD_PRODUCT,
-  ADD_PRODUCT_SUCCESS,
-  ADD_PRODUCT_ERROR,
-} from "../types";
+import { ADD_PRODUCT, ADD_PRODUCT_SUCCESS, ADD_PRODUCT_ERROR } from '../types';
 
 // Each reducer has it own state
 const initialState = {
@@ -11,8 +7,20 @@ const initialState = {
   loading: false,
 };
 
-export default function(state = initialState, action) {
-  switch(action.type) {
+// eslint-disable-next-line import/no-anonymous-default-export
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case ADD_PRODUCT:
+      return {
+        ...state,
+        loading: action.payload,
+      };
+    case ADD_PRODUCT_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        products: [...state.products, action.payload],
+      };
     default:
       return state;
   }
